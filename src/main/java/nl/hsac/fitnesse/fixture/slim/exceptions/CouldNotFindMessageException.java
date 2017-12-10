9a2566ca79871363db9d62beed831c7793cc7d85
Fried Hoeben <github@hsac.nl>
2017-12-10 12:00:02 +0100

    No need to show stack trace in wiki when no mail could be found Better name for parameter.

--- src/main/java/nl/hsac/fitnesse/fixture/slim/exceptions/CouldNotFindMessageException.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/exceptions/CouldNotFindMessageException.java
@@ -8,7 +8,7 @@ import javax.mail.Folder;
 import static java.lang.String.format;
 
 public class CouldNotFindMessageException extends SlimFixtureException {
-    public CouldNotFindMessageException(Folder inbox, EmailFixture.SearchParameters params) {
-        super(format("Could not find mail in '%s' folder with search parameters: %s", inbox.getName(), params.toString()));
+    public CouldNotFindMessageException(Folder folder, EmailFixture.SearchParameters params) {
+        super(false, format("Could not find mail in '%s' folder with search parameters: %s", folder.getName(), params.toString()));
     }
 }
